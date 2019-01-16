144d52ec09a990b73f1ce61a258ee7aad0f007dc
Leonardo <leonardolazzarin14@gmail.com>
2019-01-16 10:01:16 +0100

    Inizio progetto

---
+++ Lazzarin_S_sportApp/Lazzarin_S_sportApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lazzarin_S_sportApp
+//
+//  Created by Leonardo Lazzarin on 16/01/2019.
+//  Copyright © 2019 Leonardo Lazzarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
